390fb595ffa8b55e95ddd972a7a1f18f907802cb
Kasey Arnold <kaseyarnold15@gmail.com>
2020-04-27 16:38:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,26 +7,26 @@ class ApplicationController < Sinatra::Base
       set :public_folder, 'public'
       set :views, 'app/views'
     end
-  
+
     get '/recipes/new' do #loads new form
       erb :new
     end
-  
+
     get '/recipes' do #loads index page
       @recipes = Recipe.all
       erb :index
     end
-  
+
     get '/recipes/:id' do  #loads show page
       @recipe = Recipe.find_by_id(params[:id])
       erb :show
     end
-  
+
     get '/recipes/:id/edit' do #loads edit form
       @recipe = Recipe.find_by_id(params[:id])
       erb :edit
     end
-  
+
     patch '/recipes/:id' do  #updates a recipe
       @recipe = Recipe.find_by_id(params[:id])
       @recipe.name = params[:name]
@@ -35,18 +35,18 @@ class ApplicationController < Sinatra::Base
       @recipe.save
       redirect to "/recipes/#{@recipe.id}"
     end
-  
+
     post '/recipes' do  #creates a recipe
       @recipe = Recipe.create(params)
       redirect to "/recipes/#{@recipe.id}"
     end
-  
+
     delete '/recipes/:id' do #destroy action
       @recipe = Recipe.find_by_id(params[:id])
       @recipe.delete
       redirect to '/recipes'
     end
-  
+
 
   # code actions here!
 
